5d7b5aa7008de81bc1f06b7946586d52e05bc697
Paloma <paloma.sodhi@gmail.com>
2016-10-17 19:51:11 -0400

    added odometry motion model + map visualization function

--- scripts/MapBuilder.py
+++ scripts/MapBuilder.py
@@ -1,6 +1,10 @@
 import sys
 import numpy as np
 
+from matplotlib import pyplot as plt
+from matplotlib import figure as fig
+import time
+
 class MapBuilder:
 
     def __init__(self, src_path_map):
@@ -13,9 +17,13 @@ class MapBuilder:
         self._size_x = self._prob.shape[0]
         self._size_y = self._prob.shape[1]
 
-def main(args):
-    src_path_map = '../map/wean.dat'
-    map1 = MapBuilder(src_path_map)
+    def visualize_map(self):
+        fig = plt.figure()
+        mng = plt.get_current_fig_manager(); mng.resize(*mng.window.maxsize())
+        plt.ion(); plt.imshow(self._prob, cmap='Greys'); plt.axis([0, self._size_x, 0, self._size_y]); plt.draw()
+        time.sleep(100)
 
 if __name__=="__main__":
-    main(sys.argv)
+    
+    # src_path_map = '../map/wean.dat'
+    pass
