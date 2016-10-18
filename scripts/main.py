d014ba6acccec614d065af66659415e01a625c7f
Merritt Jenkins <merrittj@andrew.cmu.edu>
2016-10-18 16:53:52 -0400

    Updated main script
    The new main script will visualize the particles and iterate through
    timesteps using the motion model.

--- scripts/main.py
+++ scripts/main.py
@@ -1,13 +1,37 @@
 import numpy as np
+import sys
 
 from MotionModel import MotionModel
 from MapBuilder import MapBuilder
 
+from matplotlib import pyplot as plt
+from matplotlib import figure as fig
+import time
+
+#----------------------------FUNCTION TO SHOW THE MAP--------------------------------
+
+def mapInit(mapList):
+	fig = plt.figure()
+	plt.switch_backend('TkAgg') #this addition is so the display works on Merritt's computer..
+	mng = plt.get_current_fig_manager(); #mng.resize(*mng.window.maxsize())
+	mng.window.state('zoomed') #this is also so it works on Merritt's computer...
+	plt.ion(); plt.imshow(mapList, cmap='Greys'); plt.axis([0, 800, 0, 800]);
+
+
+def mapShow(mapList, X_bar):
+	x_locs = [item[0][0] for item in X_bar];
+	y_locs = [item[0][1] for item in X_bar]
+	scat = plt.scatter(x_locs, y_locs, c='r', marker='o')
+	#plt.draw()
+	plt.pause(0.05)
+	scat.remove()
+
+
+
+
 #-------------------------------PARSE THE DATA FILE----------------------------------
 #this parses robotdata1 into three lists: odometry, laser position and time, and laser readings
 
-#filename = '../robotdata1.log' 
-#filename = 'data/log/robotdata1/robotdata1.log' 
 filename = '../log/robotdata1.log' 
 
 txt = open(filename)
@@ -28,60 +52,69 @@ with open('robotdata1.log') as inputfile:
 		else:
 			s = line[2:-1]
 			floats = [float(x) for x in s.split()]
-			results_O.append([floats[0], floats[1], floats[2], floats[5]])
-			results_L_loc.append([floats[3], floats[4], floats[5]])
-			results_L.append(floats[6:-1])
+			results_O.append([floats[0], floats[1], floats[2], floats[186]]) #x_loc, y_loc, angle, timestamp
+			results_L_loc.append([floats[3], floats[4], floats[5], floats[186]]) #x_loc, y_loc, angle, timestamp
+			results_L.append(floats[6:185]) #180 laser scans
 			
-
+			
+			
 #-------------------------------MAIN SCRIPT----------------------------------
 
 #-----------------initialize variables-------------------
-alpha1 = 0.1
-alpha2 = 0.1
-alpha3 = 0.1
-alpha4 = 0.1
+alpha1 = 1
+alpha2 = 1
+alpha3 = 1
+alpha4 = 1
 mot=MotionModel(alpha1, alpha2, alpha3, alpha4)
-map=MapBuilder('../map/wean.dat') #I can't seem to call the map
 
-M=100 #number of particles
+M=1000 #number of particles
 
 #initialize particle locations
-p_x = np.random.uniform(0,800,M) #change this to match the map
-p_y = np.random.uniform(0,800,M) #change this to match the map
+p_x = np.random.randint(400,450,M) #change this to match the map
+p_y = np.random.randint(100,600,M) #change this to match the map
 p_theta = np.random.uniform(-3.14,3.14,M)
+
+map=MapBuilder('../map/wean.dat')
+mapList = map.getMap()
+mapInit(mapList)
 
 #--------------------------------------------------------
 
 for t in range(1, len(results_O)):
 
-	X_bar = [[[0,0,0], 0.0]]*M #list of lists of x,y,theta and w
+	X_bar = []
 
 	#calculate u_t
 	u_t0 = results_O[t-1][0:3] 
 	u_t1 = results_O[t][0:3] #this is just x,y,theta (no timestep)
 	
-	#calculate z_t
-	z_t = results_L[t]; #all 180 scans at each timestep
+	#calculate z_t (will use later)
+	#z_t = results_L[t]; #all 180 scans at each timestep
 	
 	for m in range(0,M):
 		#pull x_t from motion model
-		x_t0 = [p_x[0], p_y[0], p_theta[0]] #assign x_t0 as one of the particles from random initialization
+		if (t==1):
+			x_t0 = [p_x[m], p_y[m], p_theta[m]] #assign x_t0 as the particles from random initialization
+		else:
+			x_t0 = x_t1 #assign x_t0 as the old x_t
+			
 		x_t1 = mot.sample_motion_model(u_t0, u_t1, x_t0)
 		
 		#pull w_t from sensor model
 		w_t = 1.0; #set at 1 right now, all weights equal
 		
-		#update X_bar_t
-		X_bar[m][0] = x_t1   #[a + b for a, b in zip(X_bar[t][0], x_t1)]
-		X_bar[m][1] = w_t   #X_bar[t][1] + w_t
+		#update X_bar each timestep
+		list = [x_t1, w_t]
+		X_bar.append(list)
 		
 		
 	#HERE WE IMPLEMENT THE IMPORTANCE SAMPLING, BUT ERIC SAID WE SHOULD FIRST TRY WITHOUT	
 	#
 	#
 	
-print X_bar[50]	
-
-#map.visualize_map()		
-		
 	
+	mapShow(mapList, X_bar)
+
+
+
+
