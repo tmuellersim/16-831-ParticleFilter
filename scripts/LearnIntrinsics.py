ec8e95bc72d3ebd24d189d01246d6315a0da81a3
Merritt Jenkins <merrittj@andrew.cmu.edu>
2016-10-19 23:17:22 -0400

    Added fxns to SensorModel and new script LearnIntrinsics
    The SensorModel class now contains functions to automate learning the
    intrinsics. The script LearnIntrinsics simply loads the log file and
    calls the appropriate functions from SensorModel.

---
+++ scripts/LearnIntrinsics.py
@@ -0,0 +1,54 @@
+import numpy as np
+import sys
+
+from MotionModel import MotionModel
+from MapBuilder import MapBuilder
+from SensorModel import SensorModel
+
+from matplotlib import pyplot as plt
+from matplotlib import figure as fig
+import time
+
+from datetime import datetime
+
+
+# -------------------------------PARSE THE DATA FILE----------------------------------
+# this parses robotdata1 into three lists: odometry, laser position and time, and laser readings
+
+filename = '../log/robotdata1.log' 
+
+txt = open(filename)
+
+results_O = []
+results_L_loc = []
+results_L = []
+mod_results_O = []
+
+with open('robotdata1.log') as inputfile:
+    for line in inputfile:
+        if line[0] is 'O':
+            temp = 'nothing'
+            # FOR NOW WE JUST LOOK AT ODOMETRY WHEN THE LASER SCAN COMES IN
+            # s = line[2:-1]
+            # floats = [float(x) for x in s.split()]
+            # results_O.append(floats)
+
+        else:
+            s = line[2:-1]
+            floats = [float(x) for x in s.split()]
+            results_O.append([floats[0], floats[1], floats[2], floats[186]]) # x_loc, y_loc, angle, timestamp
+            results_L_loc.append([floats[3], floats[4], floats[5], floats[186]]) # x_loc, y_loc, angle, timestamp
+            results_L.append(floats[6:185]) # 180 laser scans		
+
+
+# -------------------------------MAIN SCRIPT----------------------------------
+
+sensorModel = SensorModel('../map/wean.dat')
+
+#----------------------main loop-------------------------
+
+for other in range(0,12): # THE BOOK SUGGESTS 12 ITERATIONS
+    for t in range(1, 100):       
+        (sigma_hit, lambda_short) = sensorModel.learn_params(results_L[t], results_O[t])  
+
+    print (sigma_hit, lambda_short)
--- scripts/SensorModel.py
+++ scripts/SensorModel.py
@@ -32,7 +32,8 @@ class SensorModel:
 
     def get_phit(self, z_t1, zstar_t1):
         if (z_t1 <= self._max_range):
-            eta = norm((z_t1-zstar_t1), self._sigma_hit).cdf(self._max_range)
+            eta = 1 #norm((z_t1-zstar_t1), self._sigma_hit).cdf(self._max_range)
+            #print self._sigma_hit
             probability = eta * 1.0/math.sqrt(2*math.pi*self._sigma_hit**2) * math.e**( -0.5*(z_t1-zstar_t1)**2 / self._sigma_hit**2)
         else:
             probability = 0
@@ -90,6 +91,53 @@ class SensorModel:
 
         return q
         # return math.pow(math.e, q)  # This number may still be ridiculously small, check that it isn't zero
+        
+    def get_eta(self, z_t1, zstar_t1):
+        eta = math.pow((self.get_phit(z_t1, zstar_t1) + self.get_pshort(z_t1, zstar_t1) + self.get_pmax(z_t1, zstar_t1) + self.get_prand(z_t1, zstar_t1)), -1)
+        return eta
+        
+    def learn_params(self, z_t1_arr, x_t1):
+        """
+        param[in] z_t1_arr : laser range readings [array of 180 values] at time t
+        param[in] x_t1 : particle state belief [x, y, theta] at time t [world_frame]
+        param[out] prob_zt1 : likelihood of a range scan zt1 at time t
+        """
+        
+        #the next 9 lines of code are pretty much the same as beam_range_finder_model()
+        xl_t1 = self.particle_to_laser_tf(x_t1)
+        zstar_t1_arr = self.rayTrace(xl_t1, self._map_obj, self._min_probability)
+
+        q = 1; j = 0
+        e_hit = []; e_short = []; e_max = []; e_rand = [];
+        for k in range(1, 180):
+            if k % self._subsampling != 0:
+                continue
+            z_t1 = z_t1_arr[k]
+            zstar_t1 = zstar_t1_arr[j]
+            
+            eta = self.get_eta(z_t1, zstar_t1) #this is very similar to get_pmixture()
+            e_hit.append(eta*self.get_phit(z_t1, zstar_t1))
+            e_short.append(eta*self.get_pshort(z_t1, zstar_t1))
+            e_max.append(eta*self.get_pmax(z_t1, zstar_t1))
+            e_rand.append(eta*self.get_prand(z_t1, zstar_t1))
+            j += 1
+           
+        # do we think that |Z|^-1 from lines 10 through 13 translates to (1.0/j)?
+        self._z_hit = (1.0/j)*sum(e_hit)
+        self._z_short = (1.0/j)*sum(e_short)
+        self._z_max = (1.0/j)*sum(e_max)
+        self._z_rand = (1.0/j)*sum(e_rand)
+        
+        # these are just operations for line 14 of the algorithm (is the square correct?)
+        zi_minus_zstar = [a-b for a,b in zip(z_t1_arr, zstar_t1_arr)]
+        zi_minus_zstar_square = [h*h for h in zi_minus_zstar]
+        e_mult_zdiff = [c*d for c,d in zip(e_hit, zi_minus_zstar_square)]
+        
+        #implement lines 14 and 15 of algorithm
+        self._sigma_hit = math.sqrt((1.0/sum(e_hit))*sum(e_mult_zdiff))
+        self._lambda_short = sum(e_short)/sum([a*b for a,b in zip(e_short,z_t1_arr)])
+        
+        return(self._sigma_hit, self._lambda_short)
 
     def plot_prob_zt1(self):
         prob_zt1 = []
